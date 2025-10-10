364f648d88708c53228f5ae185b85b168fef35b6
jason gokho ing <jason.gokho.ing@gmail.com>
2025-10-10 12:08:05 -0700

    Initial commit

---
+++ init_dwh.sql
@@ -0,0 +1,19 @@
+-- Create Database 'DWH'
+
+USE master;
+
+DROP DATABASE IF EXISTS DWH;
+CREATE DATABASE DWH;
+
+
+USE DWH;
+GO
+
+-- Creating Schemas within database to organize data warehouse
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
+
